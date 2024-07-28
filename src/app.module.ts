9254d301de530d07aabc3cfbbca4f42f5e294a3c
José da Silva <josesilva23p@gmail.com>
2024-07-28 15:12:25 +0100

    add docker compose for database service and setup typeORM with postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'bulir',
+      entities: [],
+      synchronize: true,
+      logging: true,
+    }),
+  ],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource) {}
+}
