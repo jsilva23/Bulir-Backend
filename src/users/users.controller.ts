5b0cbd7bd9009fe0e33d21d667232979d62eb530
José da Silva <josesilva23p@gmail.com>
2024-07-29 21:29:56 +0100

    create deposit service

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,9 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Req } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { Public } from 'src/decorators/public.decorator';
+import { User } from './entities/user.entity';
+import { DepositDto } from './dto/deposit.dto';
 
 @Controller('users')
 export class UsersController {
@@ -16,5 +18,12 @@ export class UsersController {
   @Get()
   findAll() {
     return this.usersService.findAll();
+  }
+
+  @Patch('deposit')
+  cashDeposit(@Body() depositDto: DepositDto, @Req() request: Request) {
+    const currentUser: User = request['currentUser'];
+    const newBalance = Number(depositDto.balance) + Number(currentUser.balance);
+    return this.usersService.updateUserBalance(currentUser.id, newBalance);
   }
 }
